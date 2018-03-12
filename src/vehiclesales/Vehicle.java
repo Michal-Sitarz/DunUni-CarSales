d3901bcb5be287efd96bf3a474e5df9eafaf586a
Michal-Sitarz <me@michalsitarz.comi>
2018-03-12 14:37:24 +0000

    Vehicle class

--- src/vehiclesales/Vehicle.java
+++ src/vehiclesales/Vehicle.java
@@ -1,10 +1,12 @@
 package vehiclesales;
 
 /**
- * @author MJSitarz
+ * @author Michael Sitarz
  */
+
 public abstract class Vehicle {
     private String make;
+    private String model;
     
     //default ctor
     public Vehicle(){
